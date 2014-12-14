2c2cc24d7f2a464f37c5aa17a6cb16dfafa3c037
Andre Cohen <andrecohen@Andres-MacBook-Pro.local>
2014-12-14 16:43:53 -0500

    Initial Commit

---
+++ KernGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KernGame
+//
+//  Created by Andre Cohen on 12/14/14.
+//  Copyright (c) 2014 White Shoe Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KernGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KernGame
+//
+//  Created by Andre Cohen on 12/14/14.
+//  Copyright (c) 2014 White Shoe Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
